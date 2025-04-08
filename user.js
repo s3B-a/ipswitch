eae2d8760b551a3180cd698a19dc8a301abcd259
s3B-a <161662293+s3B-a@users.noreply.github.com>
2025-04-08 13:35:00 -0500

    Create user.js
    put this in active firefox file

---
+++ user.js
@@ -0,0 +1,5 @@
+suser_pref("network.proxy.type", 1);
+user_pref("network.proxy.socks", "127.0.0.1");
+user_pref("network.proxy.socks_port", 9050);
+user_pref("network.proxy.socks_version", 5);
+user_pref("network.proxy.socks_remote_dns", true);
