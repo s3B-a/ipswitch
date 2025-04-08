67623f103e926fe596162d6df2d2498d020a05e4
s3b-a <tetr1scs0@gmail.com>
2025-04-08 16:33:04 -0500

    Updated ipswap.sh

--- ipswap.sh
+++ ipswap.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+#Finds the active firefox directory used by the user
+findProfile() {
+	usrHome=$(eval echo "~$SUDO_USER")
+	ini_file="$usrHome/.mozilla/firefox/profiles.ini"
+
+	if [ ! -f "$ini_file" ]; then
+		echo "profiles.ini not found!"
+		return 1
+	fi
+
+	activeP=$(awk -F='/^\[Profile[0-9]+\]/{section=$0} $1=="Default" && $2=="1"{print section} ' "$ini_file" | sed -E 's/\[Profile([0-9]+)\]/\1/')
+	pPath=$(awk -F= '
+        	/^\[Profile/ { in_profile=1; path=""; is_default=0 }
+        	in_profile && /^Path=/ { path=$2 }
+        	in_profile && /^Default=/ { is_default=$2 }
+        	in_profile && path && is_default=="1" {
+        	    print path; exit
+        	}
+    		' "$ini_file")
+
+	if [ -n "$pPath" ]; then
+		echo "$usrHome/.mozilla/firefox/$pPath"
+	else
+		echo "Default profiel not found!"
+		return 1
+	fi
+}
+
+#Asks for root perms
 if [ "$EUID" -ne 0 ]; then
 	echo "Run as root"
 	exec sudo "$0" "$@"
@@ -28,8 +57,19 @@ else
 	echo "$torService is running!"
 fi
 
-usrPath="~/.mozilla/firefox/v2yc8mi8.default-esr/user.js"
-fLine=$(head -n 1 "$usrPath")
+#Checking the user path as well as calling the findProfile method to find the active firefox directory
+echo "Finding profile.ini..."
+usrPath=$(findProfile)
+echo $usrPath
+
+if [ ! -f "user.js" ]; then
+	mv ./user.js $usrPath
+else
+	echo "File is already in $usrPath"
+fi
+
+echo "$usrPath/user.js"
+fLine=$(head -n 1 "$usrPath/user.js")
 
 if [["$fLine" == *'user_pref("network.proxy.type", 1);'* ]]; then
 	echo "Type 1!"
